fd5ddc8828da43c02165ffaa7b920dc8571cf128
Michelle McConville <mchelmconvl@gmail.com>
2021-01-29 17:58:36 -0600

    added header code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Header(props) {
-  
+  return (
+    <div>
+      <div className="header jumbotron-fluid" id="header">
+        <div className="container">
+          <h1><span className="text">Employee Directory</span></h1>
+          <p>Click on arrows to filter by heading or use the search box to narrow your results</p>
+        </div>
+      </div>
+      <hr></hr>
+    </div>
+  )
 }
 
 export default Header;
