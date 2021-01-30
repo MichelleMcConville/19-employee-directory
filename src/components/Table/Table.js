2bd06573fa1b2abd7dcc706cf995c11ed1ff6270
Michelle McConville <mchelmconvl@gmail.com>
2021-01-29 18:16:28 -0600

    added import DateFormat

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,6 +3,9 @@ import API from "../../util/API.js"
 import Search from "../Search/Search";
 import "./Table.css";
 
+//npm package to reformat DOB
+import DateFormat from "dateformat";
+
 function Table(props) {
   
 }
