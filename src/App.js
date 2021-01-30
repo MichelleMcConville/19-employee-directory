4ea949723b426bdc8bfe6b3903788491e956b934
Michelle McConville <mchelmconvl@gmail.com>
2021-01-29 18:29:24 -0600

    added Table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Jumbotron from "./components/Header/Header";
+import Table from "./components/Table/Table"; 
 
 function App() {
   return (
     <div>
       <header className="App-header">
         <Jumbotron/>
+        <Table />
       </header>
     </div>
   );
