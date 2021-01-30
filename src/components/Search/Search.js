a2b04498850051192bee00fc1b1c06d6c1e1832f
Michelle McConville <mchelmconvl@gmail.com>
2021-01-29 18:12:01 -0600

    added search form

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Search(props) {
-  
+  return (
+    <form>
+      <div className="form-group">
+        <div className="input-group">
+          <input 
+            onChange={props.handleInputChange} 
+            value={props.search} 
+            name="search" 
+            type="text" 
+            className="form-control" 
+            placeholder="Search here" 
+            id="search" />
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default Search;
