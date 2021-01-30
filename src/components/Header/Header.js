63c274060a1ad0ce960f600215deea1a50a11a5e
Michelle McConville <mchelmconvl@gmail.com>
2021-01-29 19:43:53 -0600

    added id=hp & re worded

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ function Header(props) {
       <div className="header jumbotron-fluid" id="header">
         <div className="container">
           <h1><span className="text">Employee Directory</span></h1>
-          <p>Click on arrows to filter by heading or use the search box to narrow your results</p>
+          <p id="hp">Click on arrows to filter by heading - OR - search by First and Last Name</p>
         </div>
       </div>
       <hr></hr>
